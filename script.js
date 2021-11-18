0ffef8330eb0df7da350e84fc6e1113c3cd36056
Naama Kaplan <nkaplany@gmail.com>
2021-11-18 12:17:58 +0100

    player can move up and down the screen, image changes to reflect movement

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //global variables: grouped by function
+//todo: refactor!
 let keys = [];
-let tic = 60;
-let position = [0, 0]; //x and y axis
+const tic = 60;
+let position = [0, 0]; //x and y axis??
 let speedMultiplier = 4;
-let jumping, falling = false;
+const vh = Math.min(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+let fishHeight = 200;
+let direction = 'right';
+
 let layers = [
     document.getElementById("layer-1"),
     document.getElementById("layer-2"),
@@ -12,10 +16,16 @@ let layers = [
     document.getElementById("layer-5"),
     document.getElementById("layer-6"),
     document.getElementById("layer-player")
-    ]
+    ];
 
-layers[0].style.backgroundPositionX = "5px";
-console.log(layers[0].style.backgroundPositionX);
+//main player object
+// class Fish {
+//     constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
+// const fish = new Fish(0,0);
 
 //add key
 document.addEventListener("keydown", function (event) {
@@ -39,40 +49,67 @@ function game () {
     updatePosition();
 }
 
-// function jump () {
-//     jumping = true
-//     if (position[1] > 300) {
-//         falling = true;
-//         position[1] -=1;
-//         if (position[1] <= 300) {
-//             jumping = falling = false;
-//         }
-//     }
-// }
+function swimUp () {
+    let fishPositionYFull = window.getComputedStyle(layers[layers.length-1]).bottom;
+    let fishPositionY = parseFloat(fishPositionYFull.slice(0, fishPositionYFull.length-2));
+    if (fishPositionY < vh - fishHeight) {
+        layers[layers.length - 1].style.transform = direction === 'right'? "rotate(-45deg)" : "scaleX(-1) rotate(-45deg)";
+        layers[layers.length - 1].style.bottom = fishPositionY + 10 +"px";
+    } else {
+        layers[layers.length - 1].style.transform = "rotate(0deg)";
+    }
+}
+
+function swimDown () {
+    let fishPositionYFull = window.getComputedStyle(layers[layers.length-1]).bottom;
+    let fishPositionY = parseFloat(fishPositionYFull.slice(0, fishPositionYFull.length-2));
+    if (fishPositionY > 0) {
+        // console.log(fishPositionY);
+        // console.log(fishHeight);
+        layers[layers.length - 1].style.transform = direction === 'right'? "rotate(45deg)" : "scaleX(-1) rotate(45deg)";
+        layers[layers.length - 1].style.bottom = fishPositionY - 10 +"px";
+    } else {
+        layers[layers.length - 1].style.transform = "rotate(0deg)";
+    }
+}
+
+
 
 function updatePosition() {
     if (keys.includes("ArrowRight")) {
         position[0] -= 1;
-        layers[layers.length -1].style.backgroundImage = "url(\"img/run.gif\")";
+        // layers[layers.length -1].style.backgroundImage = "url(\"img/SWIM_TO_RIGHT.gif\")";
         layers[layers.length -1].style.transform = "scaleX(1)";
+        direction = 'right';
     }
     if (keys.includes("ArrowLeft")) {
         position[0] += 1;
-        layers[layers.length -1].style.backgroundImage = "url(\"img/run.gif\")";
+        //layers[layers.length -1].style.backgroundImage = "url(\"img/SWIM_TO_RIGHT.gif\")";
         layers[layers.length -1].style.transform = "scaleX(-1)";
+        direction = 'left';
     }
-    if (keys.length === 0) {
-        layers[layers.length -1].style.backgroundImage = "url(\"img/idle.gif\")";
+    if (keys.includes("ArrowUp")) {
+        //position[1] += 1;
+        // layers[layers.length - 1].style.backgroundImage = "url(\"img/SWIM_TO_RIGHT.gif\")";
+        swimUp();
     }
-    if (keys.includes(" ")) {
-        layers[layers.length -1].style.backgroundImage = "url(\"img/jump.gif\")";
-        //layers[7].style.bottom = position[1] + "px";
-        //jump();
+    if (keys.includes("ArrowDown")) {
+        //position[1] += 1;
+        // layers[layers.length - 1].style.backgroundImage = "url(\"img/SWIM_TO_RIGHT.gif\")";
+        swimDown(direction);
     }
 
-    for (let i = 0; i < layers.length; i++) {
+    // if (keys.length === 0) {
+    //     layers[layers.length -1].style.backgroundImage = "url(\"img/REST_TO_RIGHT.gif\")";
+    // }
+    if (keys.includes(" ")) {
+        //todo: decide action later
+    }
+
+    //last layer is the player, who doesn't need a change in backgroundPositionX (or he'd disappear off screen)
+    for (let i = 0; i < layers.length -1 ; i++) {
         layers[i].style.backgroundPositionX = `${position[0] * ((i+1)* speedMultiplier)}px`;
-        console.log(layers[i].style.backgroundPositionX);
+        // console.log(layers[layers.length -1].style.backgroundPositionX);
     }
 }
 
