7d5f6d4152cb10ce0b16b60225c01cf547380f47
Naama Kaplan <nkaplany@gmail.com>
2021-11-19 08:34:24 +0100

    tilt functional, bounce buggy

--- script.js
+++ script.js
@@ -4,32 +4,21 @@
 let keys = [];
 const tic = 60;
 let position = [0, 0]; //x and y axis??
-let hits = 0;
 const speedMultiplier = 4;
 const vh = Math.min(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-const beeHeight = 100;
-let direction = 'right';
 
-const start = document.getElementById('button')
+const layers = document.getElementsByClassName('bg-layer');
 
-const layers = document.getElementsByClassName('layer');
-const bee = layers[layers.length-1];
-
-//main player object
-// class bee {
-//     constructor(x, y) {
-//         this.x = x;
-//         this.y = y;
-//     }
-// }
-// const bee = new bee(0,0);
-
+const bee = {
+    direction: "right",
+    height: 100,
+    layer: document.getElementById('layer-bee')
+}
 
 //add key
 document.addEventListener("keydown", function (event) {
     if (!keys.includes(event.key)) {
         keys.push(event.key);
-        console.log(keys);
     }
 })
 
@@ -43,83 +32,77 @@ document.addEventListener("keyup", function (event) {
 
 //recursive function
 function game () {
-    if (hits > 3) {
-
-    } else {
-        start.removeEventListener('click', game);
-        setTimeout(game, tic);
-        updatePosition();
-    }
-
+    document.removeEventListener('keydown', game);
+    setTimeout(game, tic);
+    updatePosition();
 }
 
 function fly (direction) {
     switch (direction) {
         case 'right':
             position[0] -= 1;
-            layers[layers.length -1].style.transform = "scaleX(1)";
+            bee.layer.style.transform = "scaleX(1)";
             break;
         case 'left':
             position[0] += 1;
-            layers[layers.length -1].style.transform = "scaleX(-1)";
+            bee.layer.style.transform = "scaleX(-1)";
             break;
     }
 }
 
+function bounce () {
+    bee.layer.classList.add('bounce');
+    setTimeout(function (){
+        bee.layer.classList.remove('bounce')
+    }, 1500)
+}
+
 function flyUp () {
-    let beePositionYFull = window.getComputedStyle(bee).bottom;
+    let beePositionYFull = window.getComputedStyle(bee.layer).bottom;
     let beePositionY = parseFloat(beePositionYFull.slice(0, beePositionYFull.length-2));
-    if (beePositionY < vh - beeHeight) {
-        bee.style.transform = direction === 'right'? "rotate(-45deg)" : "scaleX(-1) rotate(-45deg)";
-        console.log(bee.style.transform);
-        bee.style.bottom = beePositionY + 10 +"px";
+    if (beePositionY < vh - bee.height) {
+        bee.layer.style.transform = bee.direction === 'right'? "rotate(-30deg)" : "scaleX(-1) rotate(-30deg)";
+        bee.layer.style.bottom = beePositionY + 10 +"px";
     } else {
-        //todo: bounce
-        bee.style.transform = "rotate(0deg)";
+        bounce();
     }
 }
 
 function flyDown () {
-    let beePositionYFull = window.getComputedStyle(bee).bottom;
-    let beePositionY = parseFloat(beePositionYFull.slice(0, beePositionYFull.length-2)); //remove "px"
+    let beePositionYFull = window.getComputedStyle(bee.layer).bottom;
+    let beePositionY = parseFloat(beePositionYFull.slice(0, beePositionYFull.length-2));
     if (beePositionY > 0) {
-        // console.log(beePositionY);
-        // console.log(beeHeight);
-        bee.style.transform = direction === 'right'? "rotate(45deg)" : "scaleX(-1) rotate(45deg)";
-        bee.style.bottom = beePositionY - 10 +"px";
+        bee.layer.style.transform = bee.direction === 'right'? "rotate(30deg)" : "scaleX(-1) rotate(30deg)";
+        bee.layer.style.bottom = beePositionY - 10 +"px";
     } else {
-        //todo: bounce
-        console.log(beePositionY);
-        console.log(beeHeight);
-        bee.classList.add('bounce');
-        hits += 1;
-        bee.style.transform = "rotate(0deg)";
+        bounce();
+
     }
 }
 
 function updatePosition() {
-    if (keys.includes("ArrowRight") || direction === "right") {
-        direction = 'right';
+    if (keys.includes("ArrowRight") || bee.direction === "right") {
+        bee.direction = 'right';
     }
-    if (keys.includes("ArrowLeft") || direction === "left") {
-        direction = 'left';
+    if (keys.includes("ArrowLeft") || bee.direction === "left") {
+        bee.direction = 'left';
     }
+    fly(bee.direction);
     if (keys.includes("ArrowUp")) {
         flyUp();
     }
     if (keys.includes("ArrowDown")) {
-        flyDown(direction);
+        flyDown(bee.direction);
     }
 
     if (keys.includes(" ")) {
-        //todo: decide action later
+        //todo: pause?
     }
-    fly(direction);
-    //last layer is the player, who doesn't need a change in backgroundPositionX (or he'd disappear off screen)
-    for (let i = 0; i < layers.length -1 ; i++) {
-        layers[i].style.backgroundPositionX = `${position[0] * ((i+1)* speedMultiplier)}px`;
-        // console.log(layers[layers.length -1].style.backgroundPositionX);
+
+    //all but the first background layers move with increasing speed
+    for (let i = 0; i < layers.length ; i++) {
+        layers[i].style.backgroundPositionX = `${position[0] * (i * speedMultiplier)}px`;
     }
 }
 
-start.addEventListener('click', game);
+document.addEventListener('keydown', game);
